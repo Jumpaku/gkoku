e86bc3cf4280f90932c6f8f9c141f675a3fb3d2d
Jumpaku <Jumpaku@users.noreply.github.com>
2024-03-05 10:36:26 +0900

    refactor: tests for Date conversion

---
+++ calender/dayofweek.go
@@ -0,0 +1,15 @@
+package calender
+
+//go:generate stringer -type=DayOfWeek -linecomment
+type DayOfWeek int
+
+const (
+	DayOfWeekUnspecified DayOfWeek = iota // Unspecified
+	DayOfWeekMonday                       // Monday
+	DayOfWeekTuesday                      // Tuesday
+	DayOfWeekWednesday                    // Wednesday
+	DayOfWeekThursday                     // Thursday
+	DayOfWeekFriday                       // Friday
+	DayOfWeekSaturday                     // Saturday
+	DayOfWeekSunday                       // Sunday
+)
